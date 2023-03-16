88708c76e0d9ca91d3bd3ad6a92eda4d7a598ced
Roland Schütz <mail@rolandschuetz.at>
2023-03-16 07:56:58 +0100

    TASK: Switch to chat v1

--- Resources/Private/NeosUserInterface/src/manifest.tsx
+++ Resources/Private/NeosUserInterface/src/manifest.tsx
@@ -8,7 +8,7 @@ import * as React from 'react';
 import { useSelector } from 'react-redux';
 import "./style.css";
 
-manifest("CodeQ.WritingAssistant", {}, (globalRegistry, { store, frontendConfiguration }) => {	
+manifest("CodeQ.WritingAssistant", {}, (globalRegistry, { store, frontendConfiguration }) => {
 	const containerRegistry = globalRegistry.get('containers');
 	const App = containerRegistry.get('App');
 
@@ -18,20 +18,14 @@ manifest("CodeQ.WritingAssistant", {}, (globalRegistry, { store, frontendConfigu
 		const activeContentDimensions = useSelector(selectors.CR.ContentDimensions.active);
 		const interfaceLanguage = useSelector((state) => state?.user?.preferences?.interfaceLanguage);
 
-		console.log({
-			activeContentDimensions,
-			interfaceLanguage
-		});
-		// exapmle: activeContentDimensions.language[0]
-
 		return <div className="codeQ_appWrapper">
 			<App {...props} />
 			<div className={`codeQ_sideBar ${isOpen ? "codeQ_sideBar--open" : ""}`}>
 				<div className="codeQ_sideBar__title">
-					{isOpen && <Headline className="codeQ_sideBar__title--headline">Writing Assistant</Headline>}
+					{isOpen && <Headline className="codeQ_sideBar__title--headline">AI Sidekick</Headline>}
 					<IconButton icon={isOpen ? "chevron-circle-right" : "chevron-circle-left"} onClick={() => setOpen(!isOpen)} />
 				</div>
-				<iframe className="codeQ_sideBar__frame" src={"http://example.com"} />
+				<iframe className="codeQ_sideBar__frame" src={"https://docs.codeq.at/chat/?contentLanguage=" + (activeContentDimensions.language ? activeContentDimensions.language[0] : "") + "&interfaceLanguage=" + interfaceLanguage} />
 			</div>
 		</div>
 	}

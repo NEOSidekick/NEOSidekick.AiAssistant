0625b012eb8af874622e937ab49c8dc236d04c4f
Roland Schütz <mail@rolandschuetz.at>
2024-04-25 17:14:31 +0200

    TASK: Adopt asset configuration form

--- Resources/Private/BackendModule/src/Components/ConfigurationForm/AssetConfigurationForm.tsx
+++ Resources/Private/BackendModule/src/Components/ConfigurationForm/AssetConfigurationForm.tsx
@@ -6,6 +6,7 @@ import LimitField from "./LimitField";
 import BackendMessage from "../BackendMessage";
 import StartModuleButton from "../StartModuleButton";
 import AppContext from "../../AppContext";
+import SelectField from "./SelectField";
 
 export default class AssetConfigurationForm extends PureComponent {
     static contextType = AppContext
@@ -14,82 +15,66 @@ export default class AssetConfigurationForm extends PureComponent {
     {
         // This is intentionally a "==" comparison to both match null and undefined
         return (this.context.initialAppConfiguration?.onlyAssetsInUse == null ?
-            <div className={'neos-control-group'}>
-                <label className={'neos-control-label'}>
-                    {this.translationService.translate('NEOSidekick.AiAssistant:AssetModule:configuration.onlyAssetsInUse.label', 'Selection of images')}
-                </label>
-                <div className={'neos-controls'}>
-                    <select
-                        value={this.context.appConfiguration.onlyAssetsInUse}
-                        onChange={e => this.context.updateAppConfiguration({onlyAssetsInUse: e.target.value})}
-                        defaultValue={OnlyAssetsInUse.all}>
-                        <option value={OnlyAssetsInUse.all}>{this.translationService.translate('NEOSidekick.AiAssistant:AssetModule:configuration.onlyAssetsInUse.0', 'All images')}</option>
-                        <option value={OnlyAssetsInUse.onlyInUse}>{this.translationService.translate('NEOSidekick.AiAssistant:AssetModule:configuration.onlyAssetsInUse.1', 'Only images in use')}</option>
-                    </select>
-                </div>
-            </div> : null
+            <SelectField
+                label={this.translationService.translate('NEOSidekick.AiAssistant:AssetModule:configuration.onlyAssetsInUse.label', 'Selection of images')}
+                value={this.context.appConfiguration.onlyAssetsInUse}
+                defaultValue={OnlyAssetsInUse.all}
+                onChange={e => this.context.updateAppConfiguration({onlyAssetsInUse: e.target.value})}
+                options={{
+                    [OnlyAssetsInUse.all]: this.translationService.translate('NEOSidekick.AiAssistant:AssetModule:configuration.onlyAssetsInUse.0', 'All images'),
+                    [OnlyAssetsInUse.onlyInUse]: this.translationService.translate('NEOSidekick.AiAssistant:AssetModule:configuration.onlyAssetsInUse.1', 'Only images in use'),
+                }}
+            /> : null
         )
     }
 
     private renderPropertyNameField(): ReactElement
     {
-        const {configuration, initialConfiguration, updateConfiguration} = this.props;
         // This is intentionally a "==" comparison to both match null and undefined
-        return (initialConfiguration?.propertyName == null ?
-            <div className={'neos-control-group'}>
-                <label className={'neos-control-label'}>
-                    {this.translationService.translate('NEOSidekick.AiAssistant:AssetModule:configuration.propertyName.label', 'Welches Feld?')}
-                </label>
-                <div className={'neos-controls'}>
-                    <select
-                        value={configuration.propertyName}
-                        onChange={e => updateConfiguration({propertyName: e.target.value})}
-                        defaultValue={AssetPropertyName.title}>
-                        {enumKeys(AssetPropertyName).map(key =>
-                            <option value={key}>
-                                {this.translationService.translate('Neos.Media.Browser:Main:field_' + key, key)}
-                            </option>
-                        )}
-                    </select>
-                </div>
-            </div> : null
+        return (this.context.initialConfiguration?.propertyName == null ?
+            <SelectField
+                label={this.translationService.translate('NEOSidekick.AiAssistant:AssetModule:configuration.propertyName.label', 'Welches Feld?')}
+                value={this.context.appConfiguration.propertyName}
+                defaultValue={AssetPropertyName.title}
+                onChange={e => this.context.updateAppConfiguration({propertyName: e.target.value})}
+                options={enumKeys(AssetPropertyName).reduce((acc, key) => {
+                    acc[key] = this.translationService.translate('Neos.Media.Browser:Main:field_' + key, key);
+                    return acc;
+                }, {})}
+            /> : null
         )
     }
 
     private renderLanguageField(): ReactElement
     {
-        const {configuration, initialConfiguration, updateConfiguration} = this.props;
-        return (initialConfiguration?.language == null ?
-            <div className={'neos-control-group'}>
-                <label className={'neos-control-label'}>
-                    {this.translationService.translate('NEOSidekick.AiAssistant:AssetModule:configuration.language.label', 'Which language?')}
-                </label>
-                <div className={'neos-controls'}>
-                    <select
-                        value={configuration.language}
-                        onChange={e => updateConfiguration({language: e.target.value})}>
-                        {enumKeys(Language).map(language =>
-                            <option value={language}>
-                                {this.translationService.translate('NEOSidekick.AiAssistant:AssetModule:configuration.language.' + language, language)}
-                            </option>
-                        )}
-                    </select>
-                </div>
-            </div> : null
+        return (this.context.initialConfiguration?.language == null ?
+            <SelectField
+                label={this.translationService.translate('NEOSidekick.AiAssistant:AssetModule:configuration.language.label', 'Which language?')}
+                value={this.context.appConfiguration.language}
+                onChange={e => this.context.updateAppConfiguration({language: e.target.value})}
+                options={enumKeys(Language).reduce((acc, language) => {
+                    acc[language] = this.translationService.translate('NEOSidekick.AiAssistant:AssetModule:configuration.language.' + language, language);
+                    return acc;
+                }, {})}
+            /> : null
         )
     }
 
     render() {
-        const {configuration, updateConfiguration} = this.props;
         return (
             <div className={'neos-content neos-indented neos-fluid-container'}>
                 <p style={{marginBottom: '1rem'}}
                    dangerouslySetInnerHTML={{__html: this.translationService.translate('NEOSidekick.AiAssistant:AssetModule:intro', 'With this tool, you can create image descriptions and save them in the title or description field of the media asses. These descriptions are optimized as image alternative texts for SEO and accessibility. <a href="https://neosidekick.com/produkt/features/bildbeschreibungs-generator" target="_blank" style="text-decoration: underline;">Read the tutorial on how a developer can integrate them.</a>')}}/>
                 <BackendMessage identifier="bulk-image-generation"/>
+
+                <h2>{this.translationService.translate('NEOSidekick.AiAssistant:Main:selectionFilter', '')}</h2>
+                <br/>
                 {this.renderOnlyInUseField()}
                 {this.renderPropertyNameField()}
-                <LimitField configuration={configuration} updateConfiguration={updateConfiguration}/>
+                <LimitField configuration={this.context.appConfiguration} updateConfiguration={this.context.updateAppConfiguration}/>
                 {this.renderLanguageField()}
+
+
                 <div className={'neos-footer'}>
                     <StartModuleButton/>
                 </div>

4901412ecdc2fa022e9352add86bee3efab9d87f
Felix Gradinaru <felix@gradinaru.me>
2024-06-05 12:01:58 +0200

    Revert "BUGFIX: Don't use node linking service to have more fine-grained control over the hostname when generating node URIs"
    This reverts commit 48b1284473a487a1b48872245d8d7d7a9c416063.

--- Classes/Factory/FindDocumentNodeDataFactory.php
+++ Classes/Factory/FindDocumentNodeDataFactory.php
@@ -2,18 +2,13 @@
 
 namespace NEOSidekick\AiAssistant\Factory;
 
-use GuzzleHttp\Psr7\Uri;
-use InvalidArgumentException;
 use Neos\ContentRepository\Domain\Model\Node;
-use Neos\ContentRepository\Exception\NodeException;
-use Neos\ContentRepository\Exception\NodeTypeNotFoundException;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Http\Exception;
 use Neos\Flow\Mvc\Controller\ControllerContext;
 use Neos\Flow\Mvc\Routing\Exception\MissingActionNameException;
 use Neos\Flow\Persistence\Exception\IllegalObjectTypeException;
-use Neos\Neos\Domain\Model\Site;
-use Neos\Neos\Domain\Repository\SiteRepository;
+use Neos\Neos\Service\LinkingService;
 use Neos\Utility\Arrays;
 use NEOSidekick\AiAssistant\Dto\FindDocumentNodeData;
 
@@ -24,9 +19,9 @@ class FindDocumentNodeDataFactory
 {
     /**
      * @Flow\Inject
-     * @var SiteRepository
+     * @var LinkingService
      */
-    protected $siteRepository;
+    protected $nodeLinkingService;
 
     /**
      * @Flow\InjectConfiguration(path="languageDimensionName")
@@ -34,24 +29,13 @@ class FindDocumentNodeDataFactory
      */
     protected string $languageDimensionName;
 
-    /**
-     * @throws NodeException
-     * @throws \Neos\Flow\Security\Exception
-     * @throws NodeTypeNotFoundException
-     * @throws Exception
-     * @throws \Neos\Flow\Property\Exception
-     * @throws MissingActionNameException
-     * @throws IllegalObjectTypeException
-     */
     public function createFromNode(Node $node, ControllerContext $controllerContext): FindDocumentNodeData
     {
-        $site = $this->findSiteForNode($node);
-        if (!$site) {
-            throw new InvalidArgumentException(sprintf('Node "%s" has no nearby site', $node->findNodePath()));
+        try {
+            $publicUri = $this->nodeLinkingService->createNodeUri($controllerContext, $node, null, 'html', true);
+        } catch (Exception|MissingActionNameException|IllegalObjectTypeException|\Neos\Flow\Property\Exception|\Neos\Flow\Security\Exception|\Neos\Neos\Exception|\Neos\Flow\Mvc\Exception\NoMatchingRouteException $e) {
+            $publicUri = '';
         }
-
-        $publicUri = $this->buildNodeUriWithBestMatchingHost($site, $controllerContext, $node);
-
         return new FindDocumentNodeData(
             sprintf('%s-%s', $node->getNodeData()->getIdentifier(), $node->getNodeData()->getDimensionsHash()),
             $node->getContextPath(),
@@ -61,68 +45,5 @@ class FindDocumentNodeDataFactory
             // todo inspect [0] syntax... maybe we also need a mapping? replace default value and/or discuss setup with and without language dimensions
             Arrays::getValueByPath($node->getNodeData()->getDimensionValues(), $this->languageDimensionName . '.0') ?: 'de'
         );
-    }
-
-    /**
-     * @param Node $node
-     *
-     * @return Site|null
-     */
-    protected function findSiteForNode(Node $node): ?Site
-    {
-        $nodePathWithoutApex = str_replace('/sites/', '', (string)$node->findNodePath());
-        $nodePathWithoutApexAsSegmentsArray = explode('/', $nodePathWithoutApex);
-        return $this->siteRepository->findOneByNodeName($nodePathWithoutApexAsSegmentsArray[0]);
-    }
-
-    /**
-     * @param Site              $site
-     * @param ControllerContext $controllerContext
-     * @param Node              $node
-     *
-     * @return Uri|string
-     * @throws Exception
-     * @throws IllegalObjectTypeException
-     * @throws MissingActionNameException
-     */
-    protected static function buildNodeUriWithBestMatchingHost(
-        Site $site,
-        ControllerContext $controllerContext,
-        Node $node
-    ): string|Uri {
-        $firstActiveDomain = $site->getFirstActiveDomain();
-        // If there is no active domain on the closest site,
-        // we try to get an absolute uri from the routing service
-        // which uses fallback patterns
-        if (!$firstActiveDomain) {
-            return self::buildNodeUri($controllerContext, $node, true);
-        }
-
-        $currentRequestUri = $controllerContext->getRequest()->getHttpRequest()->getUri();
-        return (new Uri())
-            ->withScheme($firstActiveDomain->getScheme() ?: $currentRequestUri->getScheme())
-            ->withHost($firstActiveDomain->getHostname())
-            ->withPath(self::buildNodeUri($controllerContext, $node));
-    }
-
-    /**
-     * @param ControllerContext $controllerContext
-     * @param Node              $node
-     * @param bool              $absolute
-     *
-     * @return string
-     * @throws Exception
-     * @throws IllegalObjectTypeException
-     * @throws MissingActionNameException
-     */
-    protected static function buildNodeUri(ControllerContext $controllerContext, Node $node, bool $absolute = false): string
-    {
-        $uriBuilder = clone $controllerContext->getUriBuilder();
-        $action = $node->getContext()->getWorkspace()->isPublicWorkspace() && !$node->isHidden() ? 'show' : 'preview';
-        return $uriBuilder
-            ->reset()
-            ->setFormat('html')
-            ->setCreateAbsoluteUri($absolute)
-            ->uriFor($action, ['node' => $node], 'Frontend\Node', 'Neos.Neos');
     }
 }

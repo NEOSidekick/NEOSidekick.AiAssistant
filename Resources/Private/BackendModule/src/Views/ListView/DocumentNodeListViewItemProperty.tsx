bb2f5f1c946b40e8b3b10427b392b3daecbaabd9
Felix Gradinaru <felix@gradinaru.me>
2024-06-18 18:37:50 +0200

    Use new quality parameter for meta_description module

--- Resources/Private/BackendModule/src/Views/ListView/DocumentNodeListViewItemProperty.tsx
+++ Resources/Private/BackendModule/src/Views/ListView/DocumentNodeListViewItemProperty.tsx
@@ -69,11 +69,17 @@ export default class DocumentNodeListViewItemProperty extends PureComponent<Docu
                 )
             case 'NEOSidekick.AiAssistant/Inspector/Editors/MagicTextFieldEditor':
             case 'NEOSidekick.AiAssistant/Inspector/Editors/MagicTextAreaEditor':
+                let clonedPropertySchema = propertySchema;
+                if (propertySchema?.ui?.inspector?.editorOptions?.module === 'meta_description') {
+                    console.log('is meta_description module');
+                    clonedPropertySchema = JSON.parse(JSON.stringify(propertySchema));
+                    clonedPropertySchema.ui.inspector.editorOptions.arguments.prefer = 'quality';
+                }
                 return (
                     <TextAreaEditor
                         disabled={!this.canChangeValue()}
                         property={property}
-                        propertySchema={propertySchema}
+                        propertySchema={clonedPropertySchema}
                         item={item}
                         htmlContent={this.props.htmlContent}
                         updateItemProperty={(value: string, state: ListItemPropertyState) => this.props.updateItemProperty(value, state)}

6173bd1dc4477514b163f3a58bcecc9e635553ba
Felix Gradinaru <felix@gradinaru.me>
2024-07-30 22:26:08 +0200

    REFACTOR: Move common methods up to a new "AbstractNodeService"

---
+++ Classes/Service/AbstractNodeService.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace NEOSidekick\AiAssistant\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Internal\Hydration\IterableResult;
+use Doctrine\ORM\QueryBuilder;
+use Generator;
+use Neos\ContentRepository\Domain\Model\Node;
+use Neos\ContentRepository\Domain\Model\NodeData;
+use Neos\ContentRepository\Domain\Model\Workspace;
+use Neos\ContentRepository\Exception\NodeException;
+use Neos\Flow\Annotations as Flow;
+
+abstract class AbstractNodeService
+{
+    /**
+     * @Flow\Inject
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
+    /**
+     * @param IterableResult $iterator
+     *
+     * @return Generator
+     * @copyright Taken from and adapted: \Neos\Flow\Persistence\Doctrine\Repository::iterate()
+     *
+     */
+    protected static function iterate(IterableResult $iterator): Generator
+    {
+        foreach ($iterator as $object) {
+            $object = current($object);
+            yield $object;
+        }
+    }
+
+    /**
+     * @param array $workspaces
+     *
+     * @return QueryBuilder
+     * @copyright Taken from and adapted: \Neos\ContentRepository\Domain\Repository\NodeDataRepository::createQueryBuilder()
+     *
+     */
+    protected function createQueryBuilder(array $workspaces): QueryBuilder
+    {
+        $workspacesNames = array_map(static function (Workspace $workspace) {
+            return $workspace->getName();
+        }, $workspaces);
+
+        $queryBuilder = $this->entityManager->createQueryBuilder();
+
+        $queryBuilder->select('n')
+            ->from(NodeData::class, 'n')
+            ->where('n.workspace IN (:workspaces)')
+            ->setParameter('workspaces', $workspacesNames);
+
+        return $queryBuilder;
+    }
+
+    /**
+     * @copyright Taken from: Neos\ContentRepository\Domain\Repository\NodeDataRepository::addDimensionJoinConstraintsToQueryBuilder()
+     *
+     * If $dimensions is not empty, adds join constraints to the given $queryBuilder
+     * limiting the query result to matching hits.
+     *
+     * @param QueryBuilder $queryBuilder
+     * @param array $dimensions
+     * @return void
+     */
+    protected function addDimensionJoinConstraintsToQueryBuilder(QueryBuilder $queryBuilder, array $dimensions): void
+    {
+        $count = 0;
+        foreach ($dimensions as $dimensionName => $dimensionValues) {
+            $dimensionAlias = 'd' . $count;
+            $queryBuilder->andWhere(
+                'EXISTS (SELECT ' . $dimensionAlias . ' FROM Neos\ContentRepository\Domain\Model\NodeDimension ' . $dimensionAlias . ' WHERE ' . $dimensionAlias . '.nodeData = n AND ' . $dimensionAlias . '.name = \'' . $dimensionName . '\' AND ' . $dimensionAlias . '.value IN (:' . $dimensionAlias . ')) ' .
+                'OR NOT EXISTS (SELECT ' . $dimensionAlias . '_c FROM Neos\ContentRepository\Domain\Model\NodeDimension ' . $dimensionAlias . '_c WHERE ' . $dimensionAlias . '_c.nodeData = n AND ' . $dimensionAlias . '_c.name = \'' . $dimensionName . '\')'
+            );
+            $queryBuilder->setParameter($dimensionAlias, $dimensionValues);
+            $count++;
+        }
+    }
+
+    /**
+     * @copyright Taken from and adapted: \Neos\ContentRepository\Domain\Repository\NodeDataRepository::reduceNodeVariantsByWorkspacesAndDimensions
+     *
+     * @param array<NodeData> $nodes
+     * @param array<Workspace> $workspaces
+     *
+     * @return array<NodeData>
+     */
+    protected function reduceNodeVariantsByWorkspaces(array $nodes, array $workspaces): array
+    {
+        $reducedNodes = [];
+
+        $minimalWorkspacePositionByIdentifier = [];
+
+        $workspaceNames = array_map(
+            static function (Workspace $workspace) {
+                return $workspace->getName();
+            },
+            $workspaces
+        );
+        foreach ($nodes as $node) {
+            // Find the position of the workspace, a smaller value means more priority
+            $workspacePosition = array_search($node->getWorkspace()->getName(), $workspaceNames, true);
+            $identifier = $node->getIdentifier() . '-' . $node->getDimensionsHash();
+            // Yes, it seems to work comparing arrays that way!
+            if (!isset($minimalWorkspacePositionByIdentifier[$identifier]) || $workspacePosition < $minimalWorkspacePositionByIdentifier[$identifier]) {
+                $reducedNodes[$identifier] = $node;
+                $minimalWorkspacePositionByIdentifier[$identifier] = $workspacePosition;
+            }
+        }
+
+        return $reducedNodes;
+    }
+
+    protected function isNodeHidden(Node $node): bool
+    {
+        try {
+            $parentNode = $node->findParentNode();
+        } catch (NodeException $e) {
+            // This is thrown if no more parent node is found and that means our Node is not hidden
+            return false;
+        }
+        if ($parentNode->isHidden()) {
+            return true;
+        }
+
+        return $this->isNodeHidden($parentNode);
+    }
+}
--- Classes/Service/NodeService.php
+++ Classes/Service/NodeService.php
@@ -2,15 +2,10 @@
 
 namespace NEOSidekick\AiAssistant\Service;
 
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Internal\Hydration\IterableResult;
-use Doctrine\ORM\QueryBuilder;
-use Generator;
 use InvalidArgumentException;
 use JsonException;
 use Neos\ContentRepository\Domain\Model\Node;
 use Neos\ContentRepository\Domain\Model\NodeData;
-use Neos\ContentRepository\Domain\Model\Workspace;
 use Neos\ContentRepository\Domain\Repository\WorkspaceRepository;
 use Neos\ContentRepository\Domain\Service\NodeTypeManager;
 use Neos\ContentRepository\Domain\Utility\NodePaths;
@@ -31,17 +26,11 @@ use NEOSidekick\AiAssistant\Infrastructure\ApiFacade;
 use PDO;
 use Psr\Http\Client\ClientExceptionInterface;
 
-class NodeService
+class NodeService extends AbstractNodeService
 {
     use CreateContentContextTrait;
 
     private const BASE_NODE_TYPE = 'NEOSidekick.AiAssistant:Mixin.AiPageBriefing';
-
-    /**
-     * @Flow\Inject
-     * @var EntityManagerInterface
-     */
-    protected $entityManager;
 
     /**
      * @Flow\Inject
@@ -256,21 +245,6 @@ class NodeService
     }
 
     /**
-     * @copyright Taken from and adapted: \Neos\Flow\Persistence\Doctrine\Repository::iterate()
-     *
-     * @param IterableResult $iterator
-     *
-     * @return Generator
-     */
-    protected function iterate(IterableResult $iterator): Generator
-    {
-        foreach ($iterator as $object) {
-            $object = current($object);
-            yield $object;
-        }
-    }
-
-    /**
      * This method returns an array of all possible node types
      * that are either a document node type OR match the filtered
      * document node type, but also have our mixin as a super-type.
@@ -288,128 +262,6 @@ class NodeService
         $documentNameWithSubNodeTypes = [$documentNodeTypeFilter, ...array_keys($documentSubNodeTypes)];
         $intersectNodeTypes = array_intersect(array_values($mixinNameWithSubNodeTypes), array_values($documentNameWithSubNodeTypes));
         return array_values($intersectNodeTypes);
-    }
-
-    /**
-     * @copyright Taken from and adapted: \Neos\ContentRepository\Domain\Repository\NodeDataRepository::createQueryBuilder()
-     *
-     * @param array $workspaces
-     * @return QueryBuilder
-     */
-    protected function createQueryBuilder(array $workspaces): QueryBuilder
-    {
-        $workspacesNames = array_map(static function (Workspace $workspace) {
-            return $workspace->getName();
-        }, $workspaces);
-
-        $queryBuilder = $this->entityManager->createQueryBuilder();
-
-        $queryBuilder->select('n')
-            ->from(NodeData::class, 'n')
-            ->where('n.workspace IN (:workspaces)')
-            ->setParameter('workspaces', $workspacesNames);
-
-        return $queryBuilder;
-    }
-
-    /**
-     * @copyright Taken from: Neos\ContentRepository\Domain\Repository\NodeDataRepository::addDimensionJoinConstraintsToQueryBuilder()
-     *
-     * If $dimensions is not empty, adds join constraints to the given $queryBuilder
-     * limiting the query result to matching hits.
-     *
-     * @param QueryBuilder $queryBuilder
-     * @param array $dimensions
-     * @return void
-     */
-    protected function addDimensionJoinConstraintsToQueryBuilder(QueryBuilder $queryBuilder, array $dimensions): void
-    {
-        $count = 0;
-        foreach ($dimensions as $dimensionName => $dimensionValues) {
-            $dimensionAlias = 'd' . $count;
-            $queryBuilder->andWhere(
-                'EXISTS (SELECT ' . $dimensionAlias . ' FROM Neos\ContentRepository\Domain\Model\NodeDimension ' . $dimensionAlias . ' WHERE ' . $dimensionAlias . '.nodeData = n AND ' . $dimensionAlias . '.name = \'' . $dimensionName . '\' AND ' . $dimensionAlias . '.value IN (:' . $dimensionAlias . ')) ' .
-                'OR NOT EXISTS (SELECT ' . $dimensionAlias . '_c FROM Neos\ContentRepository\Domain\Model\NodeDimension ' . $dimensionAlias . '_c WHERE ' . $dimensionAlias . '_c.nodeData = n AND ' . $dimensionAlias . '_c.name = \'' . $dimensionName . '\')'
-            );
-            $queryBuilder->setParameter($dimensionAlias, $dimensionValues);
-            $count++;
-        }
-    }
-
-    /**
-     * @copyright Taken from and adapted: \Neos\ContentRepository\Domain\Repository\NodeDataRepository::reduceNodeVariantsByWorkspacesAndDimensions
-     *
-     * @param array<NodeData> $nodes
-     * @param array<Workspace> $workspaces
-     *
-     * @return array<NodeData>
-     */
-    protected function reduceNodeVariantsByWorkspaces(array $nodes, array $workspaces): array
-    {
-        $reducedNodes = [];
-
-        $minimalWorkspacePositionByIdentifier = [];
-
-        $workspaceNames = array_map(
-            static function (Workspace $workspace) {
-                return $workspace->getName();
-            },
-            $workspaces
-        );
-        foreach ($nodes as $node) {
-            // Find the position of the workspace, a smaller value means more priority
-            $workspacePosition = array_search($node->getWorkspace()->getName(), $workspaceNames, true);
-            $identifier = $node->getIdentifier() . '-' . $node->getDimensionsHash();
-            // Yes, it seems to work comparing arrays that way!
-            if (!isset($minimalWorkspacePositionByIdentifier[$identifier]) || $workspacePosition < $minimalWorkspacePositionByIdentifier[$identifier]) {
-                $reducedNodes[$identifier] = $node;
-                $minimalWorkspacePositionByIdentifier[$identifier] = $workspacePosition;
-            }
-        }
-
-        return $reducedNodes;
-    }
-
-    /**
-     * @copyright Taken from: Neos\Neos\Routing\FrontendNodeRoutePartHandler::getSiteByHostname()
-     *
-     * Returns a site matching the given $hostName
-     *
-     * @param string $hostName
-     *
-     * @return Site
-     * @throws NoSiteException
-     */
-    protected function getSiteByHostName(string $hostName): Site
-    {
-        $domain = $this->domainRepository->findOneByHost($hostName, true);
-        if ($domain !== null) {
-            return $domain->getSite();
-        }
-        try {
-            $defaultSite = $this->siteRepository->findDefault();
-            if ($defaultSite === null) {
-                throw new NoSiteException('Failed to determine current site because no default site is configured', 1604929674);
-            }
-        } catch (NeosException $exception) {
-            throw new NoSiteException(sprintf('Failed to determine current site because no domain is specified matching host of "%s" and no default site could be found: %s', $hostName, $exception->getMessage()), 1604860219, $exception);
-        }
-        return $defaultSite;
-    }
-
-    protected function isNodeHidden(Node $node): bool
-    {
-        try {
-            $parentNode = $node->findParentNode();
-        } catch (NodeException $e) {
-            // This is thrown if no more parent node is found and that means our Node is not hidden
-            return false;
-        }
-        if ($parentNode->isHidden()) {
-            return true;
-        }
-
-        return $this->isNodeHidden($parentNode);
     }
 
     protected function nodeMatchesLanguageDimensionFilter(FindDocumentNodesFilter $findDocumentNodesFilter, Node $node): bool

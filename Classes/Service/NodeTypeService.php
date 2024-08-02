4c117c9d088dee75d10663f39de5531455296f6a
Felix Gradinaru <felix@gradinaru.me>
2024-08-02 18:02:15 +0200

    Refactor node type service and make it more distinct, as we have a clear use case

--- Classes/Service/NodeTypeService.php
+++ Classes/Service/NodeTypeService.php
@@ -2,61 +2,98 @@
 
 namespace NEOSidekick\AiAssistant\Service;
 
-use Neos\ContentRepository\Domain\Model\NodeType;
 use Neos\ContentRepository\Domain\Service\NodeTypeManager;
 use Neos\Flow\Annotations as Flow;
 use Neos\Utility\Arrays;
+use NEOSidekick\AiAssistant\Dto\NodeTypeWithImageMetadataSchemaDto;
 
 /**
  * @Flow\Scope("singleton")
  */
 class NodeTypeService
 {
+    public const ASSET_URI_EXPRESSION = '/SidekickClientEval:\s*AssetUri\(node\.properties\.(\w+)\)/';
+    private const ALT_TAG_GENERATOR_MODULE = 'alt_tag_generator';
+
     /**
      * @Flow\Inject()
      * @var NodeTypeManager
      */
     protected $nodeTypeManager;
 
-    /**
-     * @param array $configuration
-     *
-     * @return array<NodeType>
-     */
-    public function getNodeTypesMatchingConfiguration(array $configuration): array {
+    public function getNodeTypesWithImageAlternativeTextOrTitleConfiguration(): array
+    {
         $nodeTypes = $this->nodeTypeManager->getNodeTypes();
+        $matchingNodeTypes = $this->findMatchingNodeTypes($nodeTypes);
+
+        return $this->createNodeTypeDtos($matchingNodeTypes);
+    }
+
+    private function findMatchingNodeTypes(array $nodeTypes): array
+    {
         $matchingNodeTypes = [];
-
         foreach ($nodeTypes as $nodeType) {
-            $filteredProperties = $this->filterProperties($nodeType->getProperties(), $configuration);
-            if (!empty($filteredProperties)) {
-                $matchingNodeTypes[$nodeType->getName()] = $filteredProperties;
+            if ($this->isSkippableNodeType($nodeType)) {
+                continue;
             }
+            $this->processNodeTypeProperties($nodeType, $matchingNodeTypes);
         }
-
         return $matchingNodeTypes;
     }
 
-    /**
-     * @param array $properties
-     * @param array $configuration
-     *
-     * @return array
-     */
-    private function filterProperties(array $properties, array $configuration): array {
-        return array_filter($properties, static function($propertyConfiguration, $propertyName) use ($configuration) {
-            if (str_starts_with($propertyName, '_') || str_starts_with($propertyName, 'neos_')) {
-                return false;
-            }
+    private function isSkippableNodeType($nodeType): bool
+    {
+        return $nodeType->isAbstract();
+    }
 
-            foreach ($configuration as $configurationPath => $regularExpression) {
-                $propertyConfigurationAtPath = Arrays::getValueByPath($propertyConfiguration, $configurationPath);
-                if ($propertyConfigurationAtPath === null || !preg_match($regularExpression, $propertyConfigurationAtPath)) {
-                    return false;
+    private function processNodeTypeProperties($nodeType, array &$matchingNodeTypes): void
+    {
+        foreach ($nodeType->getProperties() as $propertyName => $propertyConfiguration) {
+            if ($this->isSkippableProperty($propertyName)) {
+                continue;
+            }
+            $this->processPropertyConfiguration($nodeType, $propertyName, $propertyConfiguration, $matchingNodeTypes);
+        }
+    }
+
+    private function isSkippableProperty(string $propertyName): bool
+    {
+        return str_starts_with($propertyName, '_') || str_starts_with($propertyName, 'neos_');
+    }
+
+    private function processPropertyConfiguration($nodeType, string $propertyName, array $propertyConfiguration, array &$matchingNodeTypes): void
+    {
+        $module = Arrays::getValueByPath($propertyConfiguration, 'ui.inspector.editorOptions.module');
+        if ($module === self::ALT_TAG_GENERATOR_MODULE) {
+            $this->processAltTagGeneratorModule($nodeType, $propertyName, $propertyConfiguration, $matchingNodeTypes);
+        }
+    }
+
+    private function processAltTagGeneratorModule($nodeType, string $propertyName, array $propertyConfiguration, array &$matchingNodeTypes): void
+    {
+        $matches = [];
+        preg_match(self::ASSET_URI_EXPRESSION, Arrays::getValueByPath($propertyConfiguration, 'ui.inspector.editorOptions.arguments.url'), $matches);
+        $imageProperty = $matches[1] ?? null;
+        if ($imageProperty && array_key_exists($imageProperty, $nodeType->getProperties())) {
+            $matchingNodeTypes[$nodeType->getName()][$imageProperty]['alternativeTextPropertyName'] = $propertyName;
+        }
+    }
+
+    private function createNodeTypeDtos(array $matchingNodeTypes): array
+    {
+        $nodeTypeWithImageMetadataSchemaDtos = [];
+        foreach ($matchingNodeTypes as $nodeTypeName => $imagePropertyNames) {
+            foreach ($imagePropertyNames as $imagePropertyName => $propertyConfiguration) {
+                if ($propertyConfiguration['alternativeTextPropertyName'] || $propertyConfiguration['titleTextPropertyName']) {
+                    $nodeTypeWithImageMetadataSchemaDtos[] = new NodeTypeWithImageMetadataSchemaDto(
+                        $nodeTypeName,
+                        $imagePropertyName,
+                        $propertyConfiguration['alternativeTextPropertyName'],
+                        null
+                    );
                 }
             }
-
-            return true;
-        }, ARRAY_FILTER_USE_BOTH);
+        }
+        return $nodeTypeWithImageMetadataSchemaDtos;
     }
 }

ab8ffd11c39093886943301fe6c9a6d9ceefe248
Roland Schütz <mail@rolandschuetz.at>
2024-11-12 17:46:29 +0100

    FEATURE: Add eel helper to retrieve image text as configured for Sidekick
    (cherry picked from commit f4200784adc47e1ca78452e1fcec7b2e0842f572)

---
+++ Classes/EelHelper/NEOSidekickHelper.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace NEOSidekick\AiAssistant\EelHelper;
+
+use Exception;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\ContentRepository\Exception\NodeException;
+use Neos\Flow\Annotations as Flow;
+use Neos\Eel\ProtectedContextAwareInterface;
+
+class NEOSidekickHelper implements ProtectedContextAwareInterface
+{
+    /**
+     * @throws NodeException
+     * @throws Exception
+     */
+    public function getImageAltText(NodeInterface $node, string $propertyName): ?string
+    {
+        return $this->getImageText($node, $propertyName, 'NEOSidekick.AiAssistant/Inspector/Editors/ImageAltTextEditor');
+    }
+
+    /**
+     * @throws NodeException
+     * @throws Exception
+     */
+    public function getImageTitle(NodeInterface $node, string $propertyName): ?string
+    {
+        return $this->getImageText($node, $propertyName, 'NEOSidekick.AiAssistant/Inspector/Editors/ImageTitleEditor');
+    }
+
+    /**
+     * @throws NodeException
+     * @throws Exception
+     */
+    protected function getImageText(NodeInterface $node, string $propertyName, string $expectedEditor): ?string
+    {
+        if ($node->hasProperty($propertyName)) {
+            return $node->getProperty($propertyName);
+        }
+
+        $propertyConfiguration = $node->getNodeType()->getFullConfiguration()['properties'][$propertyName];
+        $editor = $propertyConfiguration['ui']['inspector']['editor'];
+        $editorOptions = $propertyConfiguration['ui']['inspector']['editorOptions'] ?? [];
+        $imagePropertyName = $editorOptions['imagePropertyName'] ?? null;
+        $fallbackAssetPropertyName = $editorOptions['fallbackAssetPropertyName'] ?? null;
+
+        if ($editor !== $expectedEditor) {
+            throw new Exception('NEOSidekick EelHelper expects the editor `' . $expectedEditor . '` for the property `' . $propertyName . '`, instead `' . $editor . '` is configured.');
+        }
+
+        if (!$node->hasProperty($imagePropertyName)) {
+            return null;
+        }
+
+        $image = $node->getProperty($imagePropertyName);
+        switch ($fallbackAssetPropertyName) {
+            case 'title':
+                if (!empty($image->getTitle())) {
+                    return $image->getTitle();
+                }
+                break;
+            case 'caption':
+                if (!empty($image->getCaption())) {
+                    return $image->getCaption();
+                }
+                break;
+        }
+
+        // fallback to cleaned filename
+        $resource = $image->getResource();
+        $filename = str_replace('.' . $resource->getFileExtension(), '', $resource->getFilename());
+        return str_replace('_', ' ', $filename);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function allowsCallOfMethod($methodName): bool
+    {
+        return true;
+    }
+}

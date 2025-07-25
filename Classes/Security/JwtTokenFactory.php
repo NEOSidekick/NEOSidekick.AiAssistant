f26f6193acc2ee523efbc150f16bb00f08f8a503
Felix Gradinaru <felix@gradinaru.me>
2025-07-25 13:58:52 +0200

    WIP: Add JWT authentication handling

---
+++ Classes/Security/JwtTokenFactory.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace NEOSidekick\AiAssistant\Security;
+
+use Neos\Flow\Annotations as Flow;
+use DateTime;
+use Neos\Flow\Persistence\PersistenceManagerInterface;
+use Neos\Flow\Security\AccountFactory;
+use Neos\Flow\Security\AccountRepository;
+use Neos\Flow\Security\Context;
+
+/**
+ * Class TokenFactory
+ *
+ * @package RFY\JWT\Domain\Factory
+ * @Flow\Scope("singleton")
+ */
+class JwtTokenFactory
+{
+
+    /**
+     * @var Context
+     * @Flow\Inject
+     */
+    protected $securityContext;
+
+    /**
+     * @Flow\Inject
+     * @var AccountRepository
+     */
+    protected $accountRepository;
+
+    /**
+     * @Flow\Inject
+     * @var AccountFactory
+     */
+    protected $accountFactory;
+
+    /**
+     * @Flow\Inject
+     * @var PersistenceManagerInterface
+     */
+    protected $persistenceManager;
+
+    /**
+     * @Flow\Inject()
+     * @var JwtService
+     */
+    protected $jwtService;
+
+    /**
+     * @return string
+     */
+    public function getJsonWebToken(): string
+    {
+        /** @var JwtAccount $account */
+        $account = $this->securityContext->getAccount();
+        $payload['username'] = $account->getAccountIdentifier() ?? $account->getUsername();
+        $payload['provider'] = $account->getAuthenticationProviderName();
+        $payload['creationDate'] = new DateTime();
+        $payload['expirationDate'] = (new DateTime())->modify('+1 minute');
+        return $this->jwtService->createJsonWebToken($payload);
+    }
+}
